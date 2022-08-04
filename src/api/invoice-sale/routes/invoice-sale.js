474addbde5fb8ba7ad70e591ef26921aedf8e521
rrojasn17 <rrojasn17@gmail.com>
2022-08-04 12:40:48 -0600

    Agregaron colecciones

---
+++ src/api/invoice-sale/routes/invoice-sale.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * invoice-sale router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::invoice-sale.invoice-sale');
